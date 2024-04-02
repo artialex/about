05c015960052a45399843ab0459d99946e2ba8bb
Alexey Selivanov <lazypacman@gmail.com>
2024-04-02 16:31:26 +0400

    Update

--- src/components/ShortSummary.tsx
+++ src/components/ShortSummary.tsx
@@ -2,20 +2,22 @@ export function ShortSummary() {
   return (
     <ul className="list-disc space-y-1 pl-7">
       <li>
-        I write JavaScript code professionally <em>since 2014.</em>
+        I have been professionally coding in JavaScript <em>since 2014.</em>
       </li>
       <li>
         I am a <em>T-shaped developer.</em> I like to build bridges between
-        frontend devs and backend devs, designers, QA specialists & other team
-        members
+        frontend and backend developerss, designers, QA specialists, and other
+        team members
       </li>
       <li>
-        I am a <em>Product-driven person.</em> I like to analyze things and see
-        how various metrics affect the product
+        I have a <em>Product-oriented mindset.</em> I like to work on the
+        overall product enhancement analyzing things and seeing how various
+        metrics affect the product
       </li>
       <li>
-        I use <em>modern toolset</em> to ensure the highest DX possible:
-        TypeScript, React, Next.js, Tailwind, and many others.
+        I use a <em>modern toolset</em> to optimize developer experience to the
+        full extent, using TypeScript, React, Next.js, Tailwind, and many other
+        instruments.
       </li>
     </ul>
   );
